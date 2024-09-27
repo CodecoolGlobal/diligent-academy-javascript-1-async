95897ce002acadd648a1c96db1c3318516ebe170
Daniel Hatas <21317298+samson84@users.noreply.github.com>
2024-09-27 07:20:05 +0200

    Add a promises lib.

---
+++ promises.js
@@ -0,0 +1,48 @@
+const random = (max) => Math.floor(Math.random() * (max + 1))
+
+export const sleep = (waitingTimeMs) => {
+  return new Promise((fulfill, reject) => {
+    setTimeout(() => {
+      fulfill()
+    }, waitingTimeMs)
+  })
+}
+
+export const sleepTwoSecs = () => {
+  return sleep(2000)
+}
+
+export const getRandomNumber = () => {
+  return new Promise((fulfill, reject) => {
+    setTimeout(() => {
+      const max = 10;
+      fulfill(random(max))
+    }, 0)
+  })
+}
+
+export const fail = () => {
+  return new Promise((_, reject) => {
+    setTimeout(() => reject('Ooops, it failed'), 0)
+  })
+}
+
+export const openSchrodringerBox = () => {
+  return new Promise((fulfill, reject) => {
+    setTimeout(() => {
+      const isAlive = Math.random() > 0.5
+      const catNames = ['Mirci', 'Snowball', 'Luna', 'Fluffy']
+      const selectedCat = catNames[random(catNames.length - 1)]
+      if (isAlive) {
+        fulfill(selectedCat)
+      } else {
+        reject(new Error(`Ooops, right now ${selectedCat} has only 8 lives.`))
+      }
+    }, 0)
+  })
+}
+
+export const getRandomNumberSlowly = async () => {
+  await sleep(2000);
+  return await getRandomNumber();
+}
